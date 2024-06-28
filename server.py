302aca9e740c720d784b753b00a942bc6019d9ac
Ghost-antivirus <150942394+Ghost-antivirus@users.noreply.github.com>
2024-06-28 18:27:48 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify(message="Hello, world!")
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.json
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
